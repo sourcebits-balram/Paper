5e2cbff0d3127d645889889b5a85cd6126188a44
Sourcebits-balram <balram.singh@sourcebits.com>
2014-05-23 15:23:40 +0530

    Initial Commit

---
+++ Paper/HACollectionViewSmallLayout.h
@@ -0,0 +1,15 @@
+//
+//  HACollectionViewSmallLayout.h
+//  Paper
+//
+//  Created by Heberti Almeida on 04/02/14.
+//  Copyright (c) 2014 Heberti Almeida. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HACollectionViewSmallLayout : UICollectionViewFlowLayout
+
+@property (nonatomic, weak) UIView * superView;
+
+@end
